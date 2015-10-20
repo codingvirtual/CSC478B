e7a316785cf35f27bbdb89b12e5bae44e9149a3c
Greg Palen <codingvirtual@gmail.com>
2015-10-20 17:39:58 -0500

    4: Develop FileSet Interface
    Task-Url: https://github.com/codingvirtual/csc478b/issues/issue/4
    
    Add Javadoc comments and setters/getters

--- src/core/Schedule.java
+++ src/core/Schedule.java
@@ -3,6 +3,10 @@ package core;
 import java.util.Date;
 
 /**
+ * The Schedule class represents both a specific date and time as well as a recurrence
+ * interval. 
+ * The date and time are represented by the standard Java Date class.
+ * Recurrence is specified via an Enum defined in within the class.
  * 
  * @author Greg
  * @version 1.0
@@ -11,6 +15,10 @@ import java.util.Date;
  */
 public class Schedule {
 	
+	/**
+	 * The Recurrence enum represents the frequency of recurrence of the scheduled object.
+	 * 
+	 */
 	public enum Recurrence {
 		Daily,
 		Weekly,
@@ -26,20 +34,37 @@ public class Schedule {
 		this.interval = interval;
 	}
 	
+	/**
+	 * 
+	 * @param interval Recurrence interval as specified by the Recurrence enum
+	 * @return Schedule Returns the updated Schedule object
+	 */
 	public Schedule setRecurrence(Recurrence interval) {
 		// TODO: add range and error checking
 		this.interval = interval;
 		return this;
 	}
 	
+	/**
+	 * Gets the Recurrence for this Schedule object.
+	 * @return Recurrence A Recurrence value from the enum specification
+	 */
 	public Recurrence getRecurrence() {
 		return interval;
 	}
 
+	/**
+	 * Gets the date and time set for this Schedule
+	 * @return Date Standard Java date is returned that includes a date and time.
+	 */
 	public Date getDate() {
 		return date;
 	}
 
+	/**
+	 * Sets a new date for the schedule. Should be a date in the future.
+	 * @param date Java Date object representing the new data to be set.
+	 */
 	public void setDate(Date date) {
 		this.date = date;
 	}
