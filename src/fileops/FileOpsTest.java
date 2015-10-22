51e0800f6b800ca516588786ee8e4344699dbba8
Greg Palen <codingvirtual@gmail.com>
2015-10-22 17:13:04 -0500

    13: Develop FileOps class
    Task-Url: https://github.com/codingvirtual/csc478b/issues/issue/13
    
    Extremely basic file copying works but is very, very, very fragile.
    Created a test case in order to debug it.

---
+++ src/fileops/FileOpsTest.java
@@ -0,0 +1,72 @@
+/**
+ *
+ * @author Greg Palen
+ * @version 0.1.0
+ *
+ * <h3>Revision History</h3>
+ * <p>
+ * 0.1.0	GP	Initial revision
+ * 
+ * </p>
+ */
+package fileops;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import core.FileSet;
+
+/**
+ * @author Greg
+ *
+ */
+public class FileOpsTest {
+
+	private FileSet files = new FileSet();
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		files.setDestination("/Users/Greg/Desktop/Copy/");
+		files.addPath("/Users/Greg/Documents/Udacity Classes/Developing Scalable Apps/ud859-master/Lesson_2/00_Conference_Central/src/main/webapp/index.html");
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link fileops.FileOps#FileOps(core.FileSet)}.
+	 */
+	@Test
+	public void testFileOpsCreation() {
+		FileOps testOps = new FileOps(this.files);
+		assertEquals(this.files, testOps.getFilesToCopy());
+	}
+
+	/**
+	 * Test method for {@link fileops.FileOps#run()}.
+	 */
+	@Test
+	public void testRun() {
+		FileOps testOps = new FileOps(this.files);
+		try {
+			testOps.run();
+			assertTrue(true);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			fail("IO Excpetion occurred");
+			e.printStackTrace();
+		}
+	}
+
+}
