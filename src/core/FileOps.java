f8105a266845bf61c0e7894a74b7cd798f194a4e
Greg Palen <codingvirtual@gmail.com>
2015-10-22 10:52:02 -0500

    Create FileOps class to handle the actual file backup operations

---
+++ src/core/FileOps.java
@@ -0,0 +1,17 @@
+package core;
+
+/**
+ * The FileOps class defines the operation and behavior of the entire backup operation.
+ * 
+ * <h3>Revision History</h3>
+ * <p>
+ * 0.1.0	gp	Initial build
+ * 
+ * </p>
+ * @author Greg Palen
+ * @version 0.1.0
+ *
+ */
+public class FileOps {
+
+}
