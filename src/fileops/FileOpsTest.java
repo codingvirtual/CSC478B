b414c0920335e108a96d83425c8d8b0d6eefbb6e
Greg Palen <codingvirtual@gmail.com>
2015-10-22 19:30:39 -0500

    13: Develop FileOps class
    Change tests to write destination to Google Drive via desktop client
    Task-Url: https://github.com/codingvirtual/csc478b/issues/issue/13

--- src/fileops/FileOpsTest.java
+++ src/fileops/FileOpsTest.java
@@ -33,7 +33,7 @@ public class FileOpsTest {
 	 */
 	@Before
 	public void setUp() throws Exception {
-		files.setDestination("/Users/Greg/Desktop/Copy");
+		files.setDestination("/Users/Greg/Google Drive/Copy");
 		files.addPath("/Users/Greg/Documents/Udacity Classes/Developing Scalable Apps/ud859-master/Lesson_2/00_Conference_Central/src/main/webapp/index.html");
 	}
 
