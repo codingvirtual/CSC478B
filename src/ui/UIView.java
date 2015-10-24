ddb6e1390641614b8ee0266d38dc95c12919d3bc
Greg Palen <codingvirtual@gmail.com>
2015-10-24 15:37:43 -0500

    Create Application class to represent main application context (essentially the "model" in MVC) Create UIController class to act as controller, which invokes creation of View Create UIView class (refactor prior BackupUI.java)

--- src/ui/UIView.java
+++ src/ui/UIView.java
@@ -34,8 +34,9 @@ import java.awt.Insets;
  *
  * @author robertson
  */
-public class BackupUI extends javax.swing.JFrame {
+public class UIView extends javax.swing.JFrame {
 
+	private UIController mController;
     /**
 	 * 
 	 */
@@ -43,7 +44,8 @@ public class BackupUI extends javax.swing.JFrame {
 	/**
      * Creates new form BackupUI
      */
-    public BackupUI() {
+    public UIView(UIController controller) {
+    	mController = controller;
 		getContentPane().setFont(new Font("Helvetica Neue", Font.PLAIN, 14));
         initComponents();
     }
@@ -399,7 +401,7 @@ public class BackupUI extends javax.swing.JFrame {
         jLabel1.setText("Mirror Machine (Name TBD)");
         
         JButton btnHelp = new JButton("");
-        btnHelp.setIcon(new ImageIcon(BackupUI.class.getResource("/icons/help.png")));
+        btnHelp.setIcon(new ImageIcon(UIView.class.getResource("/icons/help.png")));
         btnHelp.setContentAreaFilled(false);
         btnHelp.setBorderPainted(false);
         btnHelp.setFocusPainted(false);
@@ -487,37 +489,7 @@ public class BackupUI extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_jRadioButton7ActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            // Set System L&F
-            UIManager.setLookAndFeel(
-            UIManager.getSystemLookAndFeelClassName());
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(BackupUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(BackupUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(BackupUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(BackupUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
 
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new BackupUI().setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.ButtonGroup buttonGroup1;
