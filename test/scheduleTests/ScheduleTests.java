d2d150364b5de313ea2d33f05b811a8d079e6614
zburch <zack_burch@hotmail.com>
2015-10-28 12:02:13 -0500

    Created Schedule Test Class

---
+++ test/scheduleTests/ScheduleTests.java
@@ -0,0 +1,44 @@
+package scheduleTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ScheduleTests {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testSchedule() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetRecurrence() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRecurrence() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetDate() {
+		fail("Not yet implemented");
+	}
+
+}
