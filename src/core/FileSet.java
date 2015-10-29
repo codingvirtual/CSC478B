fb91c3d3ce9f2bdfc3a1c4523ec750bc9e48bad4
Greg Palen <codingvirtual@gmail.com>
2015-10-28 19:05:23 -0500

    FIX: change FileSet to extend DefaultListModel
    This will allow FileSet to be used directly with a JList

--- src/core/FileSet.java
+++ src/core/FileSet.java
@@ -12,13 +12,9 @@
 package core;
 
 import java.io.IOException;
-import java.util.Date;
-
 import javax.swing.DefaultListModel;
 
-import core.Schedule.Recurrence;
-
-public class FileSet {
+public class FileSet extends DefaultListModel<String> {
 
 
 	/**
@@ -27,19 +23,9 @@ public class FileSet {
 	@SuppressWarnings("unused")
 	private static final long serialVersionUID = 3146908287308357247L;
 	
-	// TODO: should this be FilePath instead? If so, we'll need a convenience method to return the list as a string instead.
-	private DefaultListModel<String> fileSet; 
 	private Schedule schedule;
 	private String destination;
 	
-
-	/**
-	 * Constructor that creates a new, empty FileSet.
-	 * 
-	 */
-	public FileSet() {
-		fileSet = new DefaultListModel<String>();
-	}
 	
 	/**
 	 * Convenience constructor that creates a new, empty FileSet with the destination path specified.
@@ -47,45 +33,9 @@ public class FileSet {
 	 * @throws IOException if the path provided in destination is invalid or unreachable.
 	 */
 	public FileSet(String destination) throws IOException {
-		fileSet = new DefaultListModel<String>();
 		this.setDestination(destination);
 	}
-	
-	/**
-	 * Adds a path to the FileSet.
-	 * @param path String representing a path to a source file or folder to include in the backup.
-	 * The string passed should be a full path specification and NOT a relative path reference.
-	 */
-	public void addPath(String path) {
-		// TODO: Error checking needs to be added to validate that the path supplied is actually reachable.
-		// TODO: Do we throw an exception if the path is bad, or instead return a Boolean and let the caller handle it?
-		// TODO: Ensure we aren't adding a duplicate. If it's a folder reference, need to check to be sure that no
-		//			parent folders are already accounted for; if it's a file, need to check that there are no parent
-		//			folders already set to back up.
-		fileSet.addElement(path);
-	}
-	
-	public Boolean removePath(String path) {
-		// will return true if the path exists and was removed. 
-		// TODO: As above, however, should we throw an exception if the path doesn't exist?
-		//
-		
-		return fileSet.removeElement(path);
-	}
 
-	public Schedule getSchedule() {
-		return schedule;
-	}
-
-	public void setSchedule(Schedule schedule) {
-		this.schedule = schedule;
-	}
-
-	
-	public void setSchedule(Date date, Recurrence interval) {
-		schedule = new Schedule(date, interval);
-	}
-	
 	public String getDestination() {
 		return destination;
 	}
@@ -95,8 +45,5 @@ public class FileSet {
 		this.destination = destination;
 	}
 
-	public DefaultListModel<String> getFileSet() {
-		return this.fileSet;
-	}
 }
 
