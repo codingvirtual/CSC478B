afcf5335895ef1ee136a1fd83b2249b301eb922f
Greg Palen <codingvirtual@gmail.com>
2015-10-30 07:30:05 -0500

    TWEAK: add console messages to allow progress debugging

--- src/fileops/FileOps.java
+++ src/fileops/FileOps.java
@@ -46,6 +46,7 @@ public class FileOps extends SwingWorker<Void, Progress> {
 	 */
 	@Override
 	protected Void doInBackground() throws Exception {		
+		System.out.println("starting backup");
 		long totalBytes = 0;
 		long completedBytes = 0;
 		int totalFiles = this.filesToCopy.getSize();
@@ -53,11 +54,21 @@ public class FileOps extends SwingWorker<Void, Progress> {
 		
 		// Create a File object from the destination path of the FileSet
 		Path destParent = Paths.get(filesToCopy.getDestination()).toRealPath(NOFOLLOW_LINKS);
+		
+		System.out.println("created destination parent directory");
+		
 		Path destination = destParent.resolve(filesToCopy.getName());
+		
+		System.out.println("created destination backup directory");
+		
 		// Check that the destination doesn't already exist and also that it is writable
 		if (Files.exists(destination) || !Files.isWritable(destParent)) throw new IOException("Destination already exists - copying aborted");
+
 		
 		Files.createDirectories(destination);
+		
+		System.out.println("created destination directory");
+		
 		ArrayList<Path> filesToCopy = new ArrayList<Path>();
 		for (int i = 0; i < totalFiles; i++) {
 			Path sourcePath = Paths.get(this.filesToCopy.get(i)).toRealPath(NOFOLLOW_LINKS);
@@ -80,7 +91,9 @@ public class FileOps extends SwingWorker<Void, Progress> {
 			try {
 				Path destPath = destination.resolve(sourcePath.toString().substring(1));
 				Files.createDirectories(destPath.getParent());
+				System.out.println("Directory created");
 				Files.copy(sourcePath, destPath);
+				System.out.println("copied " + sourcePath.toString());
 				// Update number of bytes copied
 				completedBytes += Files.size(sourcePath);
 				// Notify observers of new progress
