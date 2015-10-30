4d815894cd48b8d511f92a11cd4f5fc384eda8b2
Greg Palen <codingvirtual@gmail.com>
2015-10-30 12:16:03 -0500

    FIX: update FileOpsTest to work with concurrency

--- test/fileOpsTests/FileOpsTest.java
+++ test/fileOpsTests/FileOpsTest.java
@@ -14,20 +14,32 @@ package fileOpsTests;
 import static org.junit.Assert.*;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import core.FileSet;
 import fileops.FileOps;
+import fileops.FileOpsMessageHandler;
+import fileops.Progress;
 
 /**
 * @author Greg
 *
 */
-public class FileOpsTest {
+public class FileOpsTest implements FileOpsMessageHandler {
 
 	private FileSet files = new FileSet();
+	
+	private int progressReceived = 0;
+	private Boolean completionReceived = false;
+	final CountDownLatch latch = new CountDownLatch(1);
+	
 	/**
 	 * @throws java.lang.Exception
 	 */
@@ -45,39 +57,54 @@ public class FileOpsTest {
 	public void tearDown() throws Exception {
 	}
 
-	/**
-	 * Test method for {@link fileops.FileOps#FileOps(core.FileSet)}.
+	/* (non-Javadoc)
+	 * @see fileops.FileOpsMessageHandler#handleProgress(java.util.List)
 	 */
-	
-	
-	
-	/** Can't run this test any more. Need to figure out how to make this work.
-	 * 
+	@Override
+	public void handleProgress(List<Progress> progressItems) {
+		progressReceived = progressItems.size();
+		System.out.println("progress received");
+		for (Progress progress : progressItems) {
+			System.out.println(progress);
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see fileops.FileOpsMessageHandler#handleCompletion()
 	 */
-	
-//	@Test
-//	public void testFileOpsCreation() {
-//		FileOps testOps = new FileOps(this.files);
-//		assertEquals(this.files, testOps.getFilesToCopy());
-//	}
+	@Override
+	public void handleCompletion() {
+		System.out.println("completionReceived");
+		completionReceived = true;
+		latch.countDown();
+	}
 
 	/**
 	 * Test method for {@link fileops.FileOps#run()}.
 	 */
-	
-	/**
-	 * This test will need to be redone to work with threaded execution.
-	 */
-//	@Test
-//	public void testRun() {
-//		FileOps testOps = new FileOps(this.files);
-//		try {
-//			testOps.run();
-//			assertTrue(true);
-//		} catch (Exception e) {
-//			// TODO Auto-generated catch block
-//			fail("Running FileOps failed");
-//			e.printStackTrace();
-//		}
-//	}
+	@Test
+	public void testRun() {
+		FileOps testOps = new FileOps(this.files, this);
+		try {
+			final ExecutorService threadPool = Executors.newFixedThreadPool(1);
+			threadPool.submit(new Runnable() {
+				public void run() {
+					try {
+						testOps.run();
+					} catch (final Throwable e) {
+						e.printStackTrace();
+					}
+				}
+			});
+			latch.await();
+			System.out.println("completion: " + completionReceived);
+			System.out.println("progressReceived: " + progressReceived);
+			assertTrue(completionReceived);
+			assertTrue(progressReceived > 0);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			fail("Running FileOps failed");
+			e.printStackTrace();
+		}
+	}
 }
