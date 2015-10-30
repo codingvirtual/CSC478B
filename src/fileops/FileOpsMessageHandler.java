4395dc5da5146803303ac4ff3d2655645bdefbfc
Greg Palen <codingvirtual@gmail.com>
2015-10-30 08:01:27 -0500

    FEATURE: create handler class to make publish handling generic

---
+++ src/fileops/FileOpsMessageHandler.java
@@ -0,0 +1,25 @@
+/**
+ *
+ * @author Greg Palen
+ * @version 0.1.0
+ *
+ * <h3>Revision History</h3>
+ * <p>
+ * 0.1.0	GP	Initial revision
+ * 
+ * </p>
+ */
+package fileops;
+
+import java.util.List;
+
+/**
+ * @author Greg
+ *
+ */
+public interface FileOpsMessageHandler {
+
+	public void handleProgress(List<Progress> progressItems);
+	public void handleCompletion();
+	
+}
