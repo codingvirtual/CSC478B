2ee88ee647795e31e398bbc0c18e507d2b622b06
Greg Palen <codingvirtual@gmail.com>
2015-10-29 19:18:42 -0500

    FIX: add try/catch to setDestination calls

--- test/fileSetTests/FileSetTest.java
+++ test/fileSetTests/FileSetTest.java
@@ -17,7 +17,7 @@ import core.FileSet;
  * @author zackburch
  *
  */
-<<<<<<< HEAD
+
 public class FileSetTest {
 	
 	private String source = "/Users/zackburch/Desktop/test.txt";
@@ -59,7 +59,7 @@ public class FileSetTest {
 		try {
 			FileSet fs = new FileSet(dest);
 			assertEquals(fs.getDestination(), dest);
-		} catch (IOException e) {
+		} catch (Exception e) {
 			fail("Could not set up fileset with destination");
 			e.printStackTrace();
 		}
@@ -120,7 +120,12 @@ public class FileSetTest {
 	public void testGetDestination() {
 		FileSet fs = new FileSet();
 		assertEquals(fs.getDestination(), null);
-		fs.setDestination(dest);
+		try {
+			fs.setDestination(dest);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		assertEquals(fs.getDestination(), dest);
 	}
 
@@ -130,204 +135,30 @@ public class FileSetTest {
 	@Test
 	public void testSetDestination() {
 		FileSet fs = new FileSet();
-		fs.setDestination(dest);
+		try {
+			fs.setDestination(dest);
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 		assertEquals(fs.getDestination(), dest);
 		
 		//test that adding a new destination replaces old destination
-		fs.setDestination(destb);
+		try {
+			fs.setDestination(destb);
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 		assertEquals(fs.getDestination(), destb);
 		
 		//test adding invalid destination does not replace old destination
 		try { 
 			fs.setDestination(destFail);
-		} catch (IOException e){
+		} catch (Exception e){
 			assertFalse(fs.getDestination() == destFail);
 		}
 		
 	}
 
 }
-=======
-//public class FileSetTest {
-//	
-//	private String source = "/Users/zackburch/Desktop/test.txt";
-//	private String sourceb = "/Users/zackburch/Desktop/test2.txt";
-//	private String sourceFail = "/Users/zackburch/Desktop/testFail.txt";
-//	private String dest = "Users/zackburch/Google Drive/Test";
-//	private String destb = "Users/zackburch/Google Drive/TestB";
-//	private String destFail = "Users/zackburch/GoogleDrive/TestFail";
-//
-//	/**
-//	 * @throws java.lang.Exception
-//	 */
-//	@Before
-//	public void setUp() throws Exception {
-//		
-//	}
-//
-//	/**
-//	 * @throws java.lang.Exception
-//	 */
-//	@After
-//	public void tearDown() throws Exception {
-//	}
-//
-//	/**
-//	 * Test method for {@link core.FileSet#FileSet()}.
-//	 */
-//	@Test
-//	public void testFileSet() {
-//		FileSet fs = new FileSet();
-//		assertTrue(true);
-//	}
-//
-//	/**
-//	 * Test method for {@link core.FileSet#FileSet(java.lang.String)}.
-//	 */
-//	@Test
-//	public void testFileSetString() {
-//		try {
-//			FileSet fs = new FileSet(dest);
-//			assertEquals(fs.getDestination(), dest);
-//		} catch (IOException e) {
-//			fail("Could not set up fileset with destination");
-//			e.printStackTrace();
-//		}
-//	}
-//
-//	/**
-//	 * Test method for {@link core.FileSet#addPath(java.lang.String)}.
-//	 */
-//	@Test
-//	public void testAddPath() {
-//		String testPath;
-//		FileSet fs = new FileSet();
-//		
-//		//test adding a file
-//		fs.addPath(source);
-//		ArrayList<String> fsreturn = fs.getFileSet(); //grab newest file set
-//		assertTrue(fsreturn.contains(source));
-//		
-//		//test adding an invalid filename
-//		fs.addPath(sourceFail);
-//		fsreturn = fs.getFileSet();
-//		assertFalse(fsreturn.contains(sourceFail));
-//		
-//		//test for duplicate file paths
-//		fs.addPath(source);
-//		fsreturn = fs.getFileSet();
-//		try {
-//			testPath = fsreturn.get(1);
-//			fail("Should not allow duplicate files");
-//		} catch(IndexOutOfBoundsException e) {
-//			assertTrue(true);
-//		}
-//		
-//		//test add second file
-//		fs.addPath(sourceb);
-//		fsreturn = fs.getFileSet();
-//		assertTrue(fsreturn.contains(sourceb));
-//		
-//		//test for duplicate paths with valid path between
-//		fs.addPath(source);
-//		fsreturn = fs.getFileSet();
-//		try {
-//			testPath = fsreturn.get(2);
-//			fail("Should not allow duplicate files");
-//		} catch(IndexOutOfBoundsException e) {
-//			assertTrue(true);
-//		}
-//		
-//		
-//	}
-//
-//	/**
-//	 * Test method for {@link core.FileSet#removePath(java.io.File)}.
-//	 */
-//	@Test
-//	public void testRemovePath() {
-//		FileSet fs = new FileSet();
-//		fs.addPath(source);
-//		ArrayList<String> fsreturn = fs.getFileSet();
-//		assertTrue(fsreturn.size() == 1);
-//		fs.removePath(source);
-//		fsreturn = fs.getFileSet();
-//		assertFalse(fsreturn.contains(source));
-//		
-//		//test removePath() when no paths exist
-//		fsreturn = fs.getFileSet();
-//		assertTrue(fsreturn.size() == 0);
-//		fs.removePath(source);
-//		assertTrue(fsreturn.size() == 0);
-//		
-//	}
-//
-//	/**
-//	 * Test method for {@link core.FileSet#getSchedule()}.
-//	 */
-//	@Test
-//	public void testGetSchedule() {
-//		fail("Not yet implemented");
-//	}
-//
-//	/**
-//	 * Test method for {@link core.FileSet#setSchedule(core.Schedule)}.
-//	 */
-//	@Test
-//	public void testSetScheduleSchedule() {
-//		fail("Not yet implemented");
-//	}
-//
-//	/**
-//	 * Test method for {@link core.FileSet#setSchedule(java.util.Date, core.Schedule.Recurrence)}.
-//	 */
-//	@Test
-//	public void testSetScheduleDateRecurrence() {
-//		fail("Not yet implemented");
-//	}
-//
-//	/**
-//	 * Test method for {@link core.FileSet#getDestination()}.
-//	 */
-//	@Test
-//	public void testGetDestination() {
-//		FileSet fs = new FileSet();
-//		assertEquals(fs.getDestination(), null);
-//		fs.setDestination(dest);
-//		assertEquals(fs.getDestination(), dest);
-//	}
-//
-//	/**
-//	 * Test method for {@link core.FileSet#setDestination(java.lang.String)}.
-//	 */
-//	@Test
-//	public void testSetDestination() {
-//		FileSet fs = new FileSet();
-//		fs.setDestination(dest);
-//		assertEquals(fs.getDestination(), dest);
-//		
-//		//test that adding a new destination replaces old destination
-//		fs.setDestination(destb);
-//		assertEquals(fs.getDestination(), destb);
-//		
-//		//test adding invalid destination does not replace old destination
-//		fs.setDestination(destFail);
-//		assertFalse(fs.getDestination() == destFail);
-//	}
-//
-//	/**
-//	 * Test method for {@link core.FileSet#getFileSet()}.
-//	 */
-//	@Test
-//	public void testGetFileSet() {
-//		FileSet fs = new FileSet();
-//		ArrayList<String> fsreturn = fs.getFileSet();
-//		assertTrue(fsreturn.size() == 0);
-//		
-//		fs.addPath(source);
-//		fsreturn = fs.getFileSet();
-//		assertEquals(fsreturn.get(0), source);
-//	}
-//
-//}
->>>>>>> refs/heads/feature-develop_file_ops#13
