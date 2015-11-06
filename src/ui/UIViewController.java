201ce823e18bb9a3c873d54020de1d219a4a1ad8
Greg Palen <codingvirtual@gmail.com>
2015-11-06 16:20:09 -0600

    FIX: update call to save the FileSet
    Now calls saveDefaultFileSet in Application

--- src/ui/UIViewController.java
+++ src/ui/UIViewController.java
@@ -328,7 +328,7 @@ public class UIViewController extends JFrame implements FileOpsMessageHandler {
 					// FIXME: Ideally, show the user a dialog box and get them to pick a different name
 					e2.printStackTrace();
 				}
-				mApp.saveFileSet();
+				mApp.saveDefaultFileSet();
         	}
         });
         
