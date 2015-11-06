ab596826211b33e0b104659ccdca0329937c0e46
Greg Palen <codingvirtual@gmail.com>
2015-11-06 14:43:44 -0600

    FEATURE: enable save and read of default FileSet

--- src/app/Application.java
+++ src/app/Application.java
@@ -13,6 +13,9 @@
 package app;
 
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import javax.swing.UnsupportedLookAndFeelException;
 import ui.UIViewController;
 import core.FileSet;
@@ -30,7 +33,7 @@ public class Application {
 		// if exists, read it and set the currentFileSet to it
 		// else create a new FileSet and set it as current
 		try {
-			currentFileSet = new FileSet("Default");
+			getDefaultFileSet();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -51,30 +54,45 @@ public class Application {
 		this.currentFileSet = currentFileSet;
 	}
 	
-	public void getFileSet(String absolutePath) {
+	public void getDefaultFileSet() {
 		/**
 		 * Reads a FileSet from the specified location
 		 */
-		
+		Path defaultFileSet = Paths.get(System.getProperty("user.home"));
+		defaultFileSet.resolve("Mirror");
+		defaultFileSet.resolve("DefaultFileSet");
+		try {
+			currentFileSet = FileSet.read(defaultFileSet.toAbsolutePath().toString());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			currentFileSet = new FileSet();
+		}
 	}
 	
-	public void saveFileSet(String absolutePath) {
-		/**
-		 * Saves FileSet to a specific location
-		 */
-	}
 	
-	public void saveFileSet() {
+	public void saveDefaultFileSet() {
 		/**
 		 * Saves FileSet to the default location (whatever that may be)
 		 */
-		
+		Path appFilesDir = Paths.get(System.getProperty("user.home"));
+		appFilesDir.resolve("Mirror");
+		try {
+			FileSet.save(appFilesDir.toAbsolutePath().toString(), currentFileSet);
+			} catch (Exception e) {
+			// FIXME: generate an error if unable to set name (this shouldn't happen)
+			e.printStackTrace();
+		}
 	}
 
 	public static void main(String args[]) {
 		
 //	    try {
 //			java.awt.EventQueue.invokeAndWait(new Runnable() {
+		
+		
+		
+		
 	    	javax.swing.SwingUtilities.invokeLater(new Runnable() {
 			    public void run() {
 			        try {
@@ -85,6 +103,9 @@ public class Application {
 					}
 			    }
 			});
+	    	
+	    	
+	    	
 //		} catch (InvocationTargetException e) {
 //			// TODO Auto-generated catch block
 //			e.printStackTrace();
