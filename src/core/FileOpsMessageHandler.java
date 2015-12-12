8a48aef5c2720da77abf4c4057ae2130702d6e82
Greg Palen <codingvirtual@gmail.com>
2015-12-11 19:48:30 -0600

    DOC: add Javadoc

--- src/core/FileOpsMessageHandler.java
+++ src/core/FileOpsMessageHandler.java
@@ -2,7 +2,9 @@ package core;
 
 import java.util.List;
 
-/**
+/** Defines required methods for any class that wants to receive {@link Progress} updates from the {@link FileOps}
+ * operation as it proceeds.
+ * 
  * @author Greg Palen
  * 
  * @version 1.0.0
@@ -10,7 +12,16 @@ import java.util.List;
  */
 public interface FileOpsMessageHandler {
 
+	/**	handleProgress will periodically receive {@link Progress} objects during the copy operation.
+	 * 
+	 * @param progressItems	One or more {@link Progress} objects containing progress updates. {@link SwingWorker}
+	 * can potentially batch progress updates, hence the reason a {@link List} must be handled.
+	 */
 	public void handleProgress(List<Progress> progressItems);
+	
+	/**	handleCompletion will be called when the copy operation has fully completed and is about to return.
+	 *  The callback signifies successful completion.
+	 */
 	public void handleCompletion();
 	
 }
